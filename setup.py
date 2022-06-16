9c9033d5dec93729bd87243615e549d0ad4fd6ad
gbPagano <guilhermebpagano@gmail.com>
2022-06-16 20:19:51 -0300

    adicionando cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Análise exploratória e preditiva do Dataset - Titanic em Python',
+    author='gbpagano',
+    license='MIT',
+)
